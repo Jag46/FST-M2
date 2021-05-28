284809bd1454f8c9ada702b0adad0883a2b2ccca
Jag46 <45729717+Jag46@users.noreply.github.com>
2021-05-28 12:03:33 +0530

    Update GitHubAPI.java
    removed the access token from last commit.

--- api-project/GitHubAPI.java
+++ api-project/GitHubAPI.java
@@ -15,14 +15,14 @@ public class GitHubAPI {
 	
 	RequestSpecification requestSpec;
 	ResponseSpecification responseSpec;
-	String sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD8JCuZZ8mKdGwYSgAjU6RU2QopdGx7+45EyRdzAd1uHPi742FvNyRUUbf37xq+ZQ+UHSzZXWVpa28ASRSJUzI9qg+PeK5N7cktNGbYyh0iU1D6eOb/BA/RBpdw5tnw1mVRWXheaqK1BudE50I+6AiGp7u1ZiMoAom1wnTXOZQUJ6qBqva35Lacc+VDXD4NF31v+38G7hAB/wPShi4JrdwzJy9RT6TerDA6M9Vo7g5m/tjb4FRrmRzEFdyM08K+G2MnymbyicsC4+85lD+rcCce7G633wlh2BCVgMM4gPawxNWK7EotVQpmyUpcOKX8GcqXHn01YrsVt6yZDZGGiX2d";
+	String sshKey = "ssh-rsa XXXX";
 	int sshKeyId;
 	@BeforeClass
 	public void setUp() {
 		requestSpec = new RequestSpecBuilder()
 				.setContentType(ContentType.JSON)
 				.setBaseUri("https://api.github.com")
-				.addHeader("Authorization", "token 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")
+				.addHeader("Authorization", "token *****")
 				.build();
 	}
   
